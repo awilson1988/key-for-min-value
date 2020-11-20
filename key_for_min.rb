cf6359d462a2c454d184ee860fa73da021690998
amanda <amanda.blaze1988@gmail.com>
2020-11-20 15:55:34 -0800

    commit tree 2bc118de448d0cc78cd2fc027d3ed411e7aee3b4

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+    min_value = nil
+    min_key = nil
+    name_hash.each do |a, b|
+      if min_value == nil
+        min_value = b
+        min_key = a
+      elsif min_value > b
+        min_value = b
+        min_key = a
+      end
+    end
+    min_key
+  end
